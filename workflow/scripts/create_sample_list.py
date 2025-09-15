51f20a9604ddc7e9a30eaed54787c7b8e0e9c7e6
r1cheu <chenrulei@cemps.ac.cn>
2025-09-15 10:10:12 +0800

    feat: add father as covar

--- workflow/scripts/create_sample_list.py
+++ workflow/scripts/create_sample_list.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import pandas as pd
 
 phenotype = pd.read_csv(snakemake.config["phenotype"], sep="\t")
@@ -7,13 +9,29 @@ filtered.dropna().to_csv(snakemake.output.phenotype, sep="\t", index=False)
 filtered.iloc[:, :2].to_csv(snakemake.output.sample_list, sep="\t", index=False)
 
 
-def give_sub(x):
+def give_sub(x, halfsib):
     for sub in halfsib:
         if sub in x:
             return sub
 
 
-filtered["sub"] = filtered["IID"].apply(give_sub)
-filtered.loc[:, ["FID", "IID", "sub"]].to_csv(
-    snakemake.output.covar, sep="\t", index=False
+filtered["mother"] = filtered["IID"].apply(partial(give_sub, halfsib=halfsib))
+
+
+def get_father(x):
+    if "~" in x:
+        id_list = x.split("~")
+        for id in id_list:
+            if "GS3" in id:
+                print(id)
+                return id
+    return "NA"
+
+
+father = list(set(filtered["IID"].apply(get_father).to_list()))
+
+filtered["father"] = filtered["IID"].apply(partial(give_sub, halfsib=father))
+
+filtered.loc[:, ["FID", "IID", "mother", "father"]].to_csv(
+    snakemake.output.covar, sep="\t", index=False, na_rep="NA"
 )
