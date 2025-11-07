77dc16c9edd3e9a3abef3bbfe35d31b1945e2556
r1cheu <chenrulei@cemps.ac.cn>
2025-11-07 20:05:23 +0800

    feat: support multi-column phenotype

--- workflow/scripts/create_sample_list.py
+++ workflow/scripts/create_sample_list.py
@@ -1,12 +1,15 @@
-from functools import partial
-
 import pandas as pd
 
-phenotype = pd.read_csv(snakemake.config["phenotype"], sep="\t")
+phenotype = pd.read_csv(snakemake.config["phenotype"], sep="\t")[
+    snakemake.config["phenotype_name"]
+]
 halfsib = snakemake.params.halfsib[snakemake.wildcards.group]
 filtered = phenotype[phenotype["IID"].apply(lambda x: any(sub in x for sub in halfsib))]
 
-male = phenotype[(~phenotype["IID"].str.contains("GS1")) & (~phenotype["IID"].str.contains("GS2"))]
-result = pd.concat([filtered, male], axis=0, ignore_index=True)
+# male = phenotype[
+#     (~phenotype["IID"].str.contains("GS1")) & (~phenotype["IID"].str.contains("GS2"))
+# ]
+# result = pd.concat([filtered, male], axis=0, ignore_index=True)
+result = filtered.copy()
 result.dropna().to_csv(snakemake.output.phenotype, sep="\t", index=False)
 result.iloc[:, :2].to_csv(snakemake.output.sample_list, sep="\t", index=False)
