e0d4cb20942a6381e75dbc975e743fbcf189e75b
r1cheu <chenrulei@cemps.ac.cn>
2025-11-12 14:42:01 +0800

    fix: handle NA in phenotype

--- workflow/scripts/create_sample_list.py
+++ workflow/scripts/create_sample_list.py
@@ -10,6 +10,6 @@ filtered = phenotype[phenotype["IID"].apply(lambda x: any(sub in x for sub in ha
 #     (~phenotype["IID"].str.contains("GS1")) & (~phenotype["IID"].str.contains("GS2"))
 # ]
 # result = pd.concat([filtered, male], axis=0, ignore_index=True)
-result = filtered.copy()
-result.dropna().to_csv(snakemake.output.phenotype, sep="\t", index=False)
+result = filtered.copy().dropna()
+result.to_csv(snakemake.output.phenotype, sep="\t", index=False)
 result.iloc[:, :2].to_csv(snakemake.output.sample_list, sep="\t", index=False)
